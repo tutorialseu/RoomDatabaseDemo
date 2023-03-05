7a488f8597a8183e700d6eb6903efc1cf93837b7
kulloveth <kulloveth@gmail.com>
2023-03-05 11:02:17 +0000

    complete the insert implementation

--- app/src/main/java/eu/tutorials/roomdemo/MainActivity.kt
+++ app/src/main/java/eu/tutorials/roomdemo/MainActivity.kt
@@ -2,10 +2,47 @@ package eu.tutorials.roomdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import eu.tutorials.roomdemo.data.EmployeeDao
+import eu.tutorials.roomdemo.data.EmployeeEntity
+import eu.tutorials.roomdemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val employeeDao = (application as EmployeeApp).db?.employeeDao()
+        binding.btnAdd.setOnClickListener {
+            employeeDao?.let {
+                    dao -> addRecord(dao)
+            }
+        }
+    }
+
+    private fun addRecord(employeeDao: EmployeeDao) {
+        val name = binding.etName.text.toString()
+        val email = binding.etEmailId.text.toString()
+        if (name.isNotEmpty() && email.isNotEmpty()) {
+            lifecycleScope.launch {
+                employeeDao.insert(EmployeeEntity(name = name, email = email))
+                Toast.makeText(applicationContext, "Record saved", Toast.LENGTH_LONG).show()
+                binding.etName.text?.clear()
+                binding.etEmailId.text?.clear()
+            }
+        }else {
+            Toast.makeText(
+                applicationContext,
+                "Name or Email cannot be blank",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
