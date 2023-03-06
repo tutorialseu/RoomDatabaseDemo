b4f87dc2a3c8e452c5c86346385a70b1e842c357
kulloveth <kulloveth@gmail.com>
2023-03-06 14:10:24 +0000

    add update and delete feature

--- app/src/main/java/eu/tutorials/roomdemo/MainActivity.kt
+++ app/src/main/java/eu/tutorials/roomdemo/MainActivity.kt
@@ -1,15 +1,18 @@
 package eu.tutorials.roomdemo
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import eu.tutorials.roomdemo.data.EmployeeDao
 import eu.tutorials.roomdemo.data.EmployeeEntity
 import eu.tutorials.roomdemo.databinding.ActivityMainBinding
+import eu.tutorials.roomdemo.databinding.DialogUpdateBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -32,19 +35,29 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             employeeDao?.fetchAllEmployee()?.collect {
                 Log.d("employee", "$it")
-                setupListOfDataIntoRecyclerView(it.toMutableList())
+                setupListOfDataIntoRecyclerView(it.toMutableList(), employeeDao)
             }
         }
     }
 
 
     private fun setupListOfDataIntoRecyclerView(
-        employeesList: MutableList<EmployeeEntity>,
+        employeesList: MutableList<EmployeeEntity>, employeeDao: EmployeeDao
     ) {
 
         if (employeesList.isNotEmpty()) {
             // Adapter class is initialized and list is passed in the param.
-            val itemAdapter = ItemAdapter(employeesList)
+            val itemAdapter = ItemAdapter(employeesList, updateListener = {
+                updateRecordDialog(it, employeeDao)
+            }) { deleteId ->
+                lifecycleScope.launch {
+                    employeeDao.fetchEmployeeById(deleteId).collect {
+                        if (it != null) {
+                            deleteRecord(deleteId, employeeDao, it.name)
+                        }
+                    }
+                }
+            }
             // Set the LayoutManager that this RecyclerView will use.
             binding.rvItemsList.layoutManager = LinearLayoutManager(this)
             // adapter instance is set to the recyclerview to inflate the items.
@@ -75,5 +88,69 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_LONG
             ).show()
         }
+    }
+
+    fun updateRecordDialog(id: Int, employeeDao: EmployeeDao) {
+        val updateDialog = Dialog(this, R.style.Theme_Dialog)
+        updateDialog.setCancelable(false)
+        val binding = DialogUpdateBinding.inflate(layoutInflater)
+        updateDialog.setContentView(binding.root)
+
+        lifecycleScope.launch {
+            employeeDao.fetchEmployeeById(id).collect {
+                binding.etUpdateName.setText(it.name)
+                binding.etUpdateEmailId.setText(it.email)
+            }
+        }
+
+        binding.tvUpdate.setOnClickListener {
+            val name = binding.etUpdateName.text.toString()
+            val email = binding.etUpdateEmailId.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty()) {
+                lifecycleScope.launch {
+                    employeeDao.update(EmployeeEntity(id, name, email))
+                    Toast.makeText(applicationContext, "Record Updated.", Toast.LENGTH_LONG)
+                        .show()
+                    updateDialog.dismiss() // Dialog will be dismissed
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Name or Email cannot be blank",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        updateDialog.show()
+    }
+
+    fun deleteRecord(id: Int, employeeDao: EmployeeDao, name: String) {
+        val builder = AlertDialog.Builder(this)
+        //set title for alert dialog
+        builder.setTitle("Delete Record")
+        //set message for alert dialog
+        builder.setMessage("Are you sure you wants to delete $name.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton("Yes") { dialogInterface, _ ->
+            lifecycleScope.launch {
+                employeeDao.delete(EmployeeEntity(id))
+                Toast.makeText(
+                    applicationContext,
+                    "Record deleted successfully.",
+                    Toast.LENGTH_LONG
+                ).show()
+                dialogInterface.dismiss() // Dialog will be dismissed
+            }
+        }
+        builder.setNegativeButton("No") { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false) // Will not allow user to cancel after clicking on remaining screen area.
+        alertDialog.show()  // show the dialog to UI
     }
 }
--- app/src/main/java/eu/tutorials/roomdemo/data/EmployeeDao.kt
+++ app/src/main/java/eu/tutorials/roomdemo/data/EmployeeDao.kt
@@ -1,17 +1,18 @@
 package eu.tutorials.roomdemo.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EmployeeDao {
-
     @Insert
     suspend fun insert(employeeEntity: EmployeeEntity)
-
     @Query("Select * from `employee-table`")
     fun fetchAllEmployee(): Flow<List<EmployeeEntity>>
+    @Query("Select * from `employee-table` where id=:id")
+    fun fetchEmployeeById(id:Int):Flow<EmployeeEntity>
+    @Update
+    suspend fun update(employeeEntity: EmployeeEntity)
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
 }
